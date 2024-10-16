df474a85c2a9ebab865bcef4b0f2bf4082bd36e1
Manolo Gómez <manologomez@correo.com>
2024-10-16 18:17:23 +0200

    Resta. Comentario editado.

--- src/es/comares/daw/ed/Calculadora.java
+++ src/es/comares/daw/ed/Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		System.out.println("Calculadora");
 		System.out.println("1. Suma");
+		System.out.println("2. Resta");
 	}
 
 }
